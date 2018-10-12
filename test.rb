05b3f150621b15fb15e2e234021f5043400a1c43
sunilkumarnagoore <43987236+sunilkumarnagoore@users.noreply.github.com>
2018-10-12 18:00:49 +0530

    this2

--- test.rb
+++ test.rb
@@ -1,8 +1,8 @@
 x = 3
-if x > 2
+if x > 5
    puts "x is greater than 2"
-elsif x <= 2 and x!=0
-   puts "x is 1"
+elsif x <= 4 and x!=0
+   puts "x is 7"
 else
    puts "I can't guess the number"
    puts "i can guess the number"
